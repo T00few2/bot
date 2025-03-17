881ab89b211227a60efb1cdd15ee894fd8b8eec2
T00few2 <christian.kjar@gmail.com>
2025-03-17 19:15:21 +0100

    REST fix

--- bot.js
+++ bot.js
@@ -173,7 +173,7 @@ const commands = [
 ].map(command => command.toJSON());
 
 // 2️⃣ Register Slash Commands
-const { REST, Routes } = require("discord.js");
+
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN);
 (async () => {
   try {
